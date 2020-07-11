5e3fa701c9261e6700b1be63e8f75a4743ea0992
adityagore1993 <adityagore.1993@hotmail.com>
2020-07-12 04:02:19 +0530

    User signup endpoint implemented

--- quora-api/target/generated-sources/com/upgrad/quora/api/model/SignoutResponse.java
+++ quora-api/target/generated-sources/com/upgrad/quora/api/model/SignoutResponse.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * SignoutResponse
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-07-05T13:00:07.199+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-07-12T03:52:23.722+05:30")
 
 public class SignoutResponse   {
   @JsonProperty("id")
